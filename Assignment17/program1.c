93c4919eee6acb4f4f7fba2b3cd7f780321f3c65
Bhavesh Narayan Gaikwad <168211950+Bhavesh2011@users.noreply.github.com>
2025-05-26 22:22:10 +0530

    Add files via upload

---
+++ Assignment17/program1.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+
+void Pattern(int iRow, int iCol)
+{
+    int i = 0;
+    int j = 0;
+
+    for(i = 1; i <= iRow; i++)
+    {
+        for(j = 1; j <= iCol; j++)
+        {
+            if((i + j) == 5)
+            {
+                printf("#\t");
+            }
+            else
+            {
+                printf("*\t");
+            }
+            
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+    int iValue1 = 0;
+    int iValue2 = 0;
+
+    printf("Enter The Number of rows : \n");
+    scanf("%d",&iValue1);
+
+    printf("Enter The Number of Columns : \n");
+    scanf("%d",&iValue2);
+
+    Pattern(iValue1, iValue2);
+
+    return 0;
+}
